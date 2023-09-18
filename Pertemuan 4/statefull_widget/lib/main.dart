775fac1c4fd4f2e6c160aa041ab2ee9d24168281
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-09-18 09:44:50 +0700

    first commit for statefull widget

--- Pertemuan 4/statefull_widget/lib/main.dart
+++ Pertemuan 4/statefull_widget/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,24 +13,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(8.0),
-          color: Colors.grey[400],
-          child: ListView(
-            children: <Widget>[
-              Container(
-                child: Text("Tes1"),
-              ),
-              Container(
-                child: Text("child2"),
-              )
-            ],
+          appBar: AppBar(
+            title: Text("Konverter Suhu"),
           ),
-        ),
-      ),
+          body: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                        labelText: "Masukkan Suhu Dalam Celcius"),
+                  ),
+                ),
+                Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [Text("tes"), Text("tes")],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [Text("tes"), Text("tes")],
+                          ),
+                        ),
+                      ]),
+                ),
+                Container(
+                  child: Text("tes"),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
