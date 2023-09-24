76ce3364da7c1c09a62d25b0ea82592ba5bca393
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-09-24 23:06:36 +0700

    add markdown

---
+++ Pertemuan 4/tugas_konversisuhu/hasil.md
@@ -0,0 +1,4 @@
+# hasil screenshot
+
+![hasil screenshot gambar 1](assets/images/hasil1.jpeg)
+![hasil screenshot gambar 2](assets/images/hasil2.jpeg)
--- Pertemuan 4/tugas_konversisuhu/lib/main.dart
+++ Pertemuan 4/tugas_konversisuhu/lib/main.dart
@@ -48,10 +48,13 @@ class _MyHomePage extends State<MyHomePage> {
   void dropDownChanged(String? changeValue) {
     setState(() {
       _newValue = changeValue!;
+
       if (_newValue == "Kelvin") {
         _result = double.parse(inputController.text) + 273;
+        listViewItem.add("Kelvin: $_result");
       } else {
         _result = double.parse(inputController.text) * 4 / 5;
+        listViewItem.add("Reamor: $_result");
       }
     });
   }
@@ -60,13 +63,17 @@ class _MyHomePage extends State<MyHomePage> {
     setState(() {
       if (_newValue == "Kelvin") {
         _result = double.parse(inputController.text) + 273;
+        listViewItem.add("Kelvin: $_result");
       } else {
         _result = double.parse(inputController.text) * 4 / 5;
+        listViewItem.add("Reamor: $_result");
       }
     });
   }
 
   var listItem = ["Kelvin", "Reamor"];
+
+  List<String> listViewItem = [];
 
   double _inputUser = 0;
   double _kelvin = 0;
@@ -80,72 +87,122 @@ class _MyHomePage extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Konversi Suhu"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(8),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Container(
-                child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    controller: inputController,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    decoration: InputDecoration(labelText: 'Input Suhu'))),
-            SizedBox(height: 30),
-            Container(
-              padding: EdgeInsets.all(6),
-              width: 200,
-              child: DropdownButton<String>(
-                  isExpanded: true,
-                  items: listItem.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  value: _newValue,
-                  onChanged: (String? changeValue) {
-                    dropDownChanged(changeValue);
-                  }),
-            ),
-            Container(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Input(inputController: inputController),
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(6),
+                width: 200,
+                child: DropdownButton<String>(
+                    isExpanded: true,
+                    items: listItem.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    value: _newValue,
+                    onChanged: (String? changeValue) {
+                      dropDownChanged(changeValue);
+                    }),
+              ),
+              Result(result: _result),
+              Container(
                 margin: EdgeInsets.only(top: 20),
-                child: Column(
-                  children: [
-                    Text(
-                      "Hasil",
-                      style: TextStyle(fontSize: 30),
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {
+                    convertTemperatur(_newValue);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.only(top: 15, bottom: 15),
+                  ),
+                  child: const Text(
+                    'Konversi Suhu',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
                     ),
-                    SizedBox(height: 10.0),
-                    Text("$_result", style: TextStyle(fontSize: 22))
-                  ],
-                )),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  convertTemperatur(_newValue);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: EdgeInsets.only(top: 15, bottom: 15),
-                ),
-                child: const Text(
-                  'Konversi Suhu',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
                   ),
                 ),
               ),
-            ),
-            Container()
-          ],
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Column(children: [
+                  Text(
+                    "Riwayat Konversi",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    height: 200,
+                    child: ListView.builder(
+                      itemCount: listViewItem.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(listViewItem[index]),
+                        );
+                      },
+                    ),
+                  ),
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Result extends StatelessWidget {
+  const Result({
+    super.key,
+    required double result,
+  }) : _result = result;
+
+  final double _result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          children: [
+            Text(
+              "Hasil",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 10.0),
+            Text("$_result", style: TextStyle(fontSize: 22))
+          ],
+        ));
+  }
+}
+
+class Input extends StatelessWidget {
+  const Input({
+    super.key,
+    required this.inputController,
+  });
+
+  final TextEditingController inputController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: TextFormField(
+            keyboardType: TextInputType.number,
+            controller: inputController,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+            decoration: InputDecoration(labelText: 'Input Suhu')));
   }
 }
