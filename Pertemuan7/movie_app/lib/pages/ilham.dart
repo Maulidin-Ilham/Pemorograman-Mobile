015b48a80cd8ca83144a8dc379615e2a27886adf
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-10-09 21:22:01 +0700

    add some changes in movie list

---
+++ Pertemuan7/movie_app/lib/pages/ilham.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ilham extends StatelessWidget {
+  const Ilham({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
